873ed8a0a9cf0c9a3af07ab5e926eac91d68ed84
Khalil El Kadiri <khalil.el-kadiri@etudiant.univ-reims.fr>
2026-05-04 14:01:21 +0200

    feat : added program to launch a vLLM server

---
+++ server/runvLLM.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+#SBATCH --account=${SBATCH_ACCOUNT}
+#SBATCH --job-name=${SBATCH_JOB_NAME}
+#SBATCH --output=${PROJECT_DIR}/logs/vLLM/run_venv-%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --time=01:30:00
+#SBATCH --constraint=armgpu
+
+# Load environnement
+romeo_load_armgpu_env
+spack load python@3.13.0/rom
+spack load cuda@12.6.3
+
+# Activating vLLM env from its venv path 
+source ${VENV_PATH}/bin/activate
+
+# Downloading gradio to get a web chat
+pip install gradio openai
+pip install python-dotenv
+
+# Cache redirection
+mkdir -p "${PROJECT_DIR}/vllm_cache"
+mkdir -p "${PROJECT_DIR}/vllm_cache/tmp" "${PROJECT_DIR}/vllm_cache/triton" "${PROJECT_DIR}/vllm_cache/hf" "${PROJECT_DIR}/vllm_cache/config"
+
+export TMPDIR="${PROJECT_DIR}/vllm_cache/tmp"
+export TRITON_CACHE_DIR="${PROJECT_DIR}/vllm_cache/triton"
+export HF_HOME="${PROJECT_DIR}/vllm_cache/hf"
+export VLLM_CONFIG_ROOT="${PROJECT_DIR}/vllm_cache/config"
+export MULTIPROCESSING_TMPDIR="${PROJECT_DIR}/vllm_cache/tmp"
+
+
+# Server launch
+/usr/bin/env \
+    TMPDIR="${PROJECT_DIR}/vllm_cache/tmp" \
+    HF_HOME="${PROJECT_DIR}/vllm_cache/hf" \
+    TRITON_CACHE_DIR="${PROJECT_DIR}/vllm_cache/triton" \
+    VLLM_CONFIG_ROOT="${PROJECT_DIR}/vllm_cache/config" \
+    PYTHONPATH=$PYTHONPATH \
+    python -m vllm.entrypoints.openai.api_server \
+        --model ${PROJECT_DIR}/models/${MODEL_FILE} \
+        --host 0.0.0.0 \
+        --port 8080 \
+        --dtype float16 \
+        --enforce-eager \
+        --gpu-memory-utilization 0.7 \
+        --max-model-len 2048 
