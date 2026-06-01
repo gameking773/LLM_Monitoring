1917c21f7822efa31665d18a4e15aeebd4c82e5e
Khalil El Kadiri <khalil.el-kadiri@etudiant.univ-reims.fr>
2026-06-01 14:14:38 +0200

    doc : commentaries added

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #SBATCH --account=${SBATCH_ACCOUNT}
 #SBATCH --job-name=${SBATCH_JOB_NAME}
-#SBATCH --output=${LLM_DIR}/logs/vLLM/setup-%j.out
+#SBATCH --output=${LLM_DIR}/logs/setup/setup-%j.out
 #SBATCH --gres=gpu:2
 #SBATCH --cpus-per-task=16
 #SBATCH --mem=128G
@@ -18,35 +18,38 @@ fi
 
 export ENGINE=$1
 
-if [ ! -d "${LLM_DIR}/lighteval_env" ] || [ ! -d "${LLM_DIR}/monitor_env" ]; then
+# if the monitoring and benchmarking env aren't installed, install them
+if [ ! -d "${LLM_DIR}/promptfoo_env" ] || [ ! -d "${LLM_DIR}/monitor_env" ]; then
     spack load python@3.12.5
     
     # python monitoring env
     if [ ! -d "${LLM_DIR}/monitor_env" ]; then
         python -m venv ${LLM_DIR}/monitor_env
         source ${LLM_DIR}/monitor_env/bin/activate
-        pip install fastapi uvicorn requests dotenv
+        pip install fastapi uvicorn requests dotenv psutil
         deactivate
     fi
 
-    # lighteval env
-    if [ ! -d "${LLM_DIR}/lighteval_env" ]; then
-        python -m venv ${LLM_DIR}/lighteval_env
-        source ${LLM_DIR}/lighteval_env/bin/activate
+    # lm-eval env
+    if [ ! -d "${LLM_DIR}/lm-eval_env" ]; then
+        python -m venv ${LLM_DIR}/lm-eval_env
+        source ${LLM_DIR}/lm-eval_env/bin/activate
         pip install --upgrade pip
-        pip install lighteval
-        deactivate
+        pip install lm-eval lm-eval[api]
+        pip install datasets
+        deactivate 
     fi
     spack unload python@3.12.5
 fi
 
+# Installation of the selected engine
 if [ "$ENGINE" == "llamacpp" ]; then
     spack load python@3.13.0/rom
     spack load cuda@13.0.2
     source "${LLM_DIR}/setup/setupLlama.sh"
 elif [ "$ENGINE" == "vllm" ]; then
     spack load python@3.12.5
-    spack load cuda@12.6.2 
+    spack load cuda@13.0.2
     source "${LLM_DIR}/setup/setupvLLM.sh"
 else
     echo "Invalid engine '$ENGINE'. You must choose between vllm and llamacpp"
